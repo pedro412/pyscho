0b01fe1196b5717e5ac22c24e4eee37397e0def8
Pedro Alvarez <pedro.alvarez@datagran.io>
2020-05-30 16:42:33 -0500

    [ADD]: Firebase json and init

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Form from '../components/Form/Form'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi there!</h1>
+    <p>Welcome to your new Gatsby site Vimind.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
